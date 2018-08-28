6c7e59c081ba80a204dc2be856f8fd4877fe5aeb
Nimrod O. Abrigo <abrigonimrod@gmail.com>
2018-08-28 17:45:22 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+let bodyparser = require('body-parser');
+
+//routes
+let userroute = require('./routes/userRoute');
+
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.use('/user',userroute);
+
+app.listen(3000,()=>console.log('node server is working'));
