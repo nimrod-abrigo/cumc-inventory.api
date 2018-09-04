46c737c59e45e41adcbde17a65aebfc0b887f17e
Nimrod O. Abrigo <abrigonimrod@gmail.com>
2018-09-04 16:29:15 +0800

    add event route

---
+++ routes/eventRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const eventController = require('../controller/eventController');
+
+router.get('/:id',eventController.getEventInfo);
+router.get('',eventController.getEvents);
+router.put('/:id',eventController.editEvent);
+router.post('',eventController.addEvent);
+router.delete('/:id',eventController.deleteEvent);
